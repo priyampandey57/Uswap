3162157cde4df9c87035eee581373d970499fbd8
punitjain9615 <punit_j@mt.iitr.ac.in>
2022-03-09 12:09:22 +0530

    Version changed in master

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
 
- solidity: "0.8.4",
+ solidity: "0.8.5",
   networks: {
      hardhat: {
        forking: {
